33a8474141dd191dcd06df1d0e14aaa12d827f38
John Dorado <146222794+Doradz22@users.noreply.github.com>
2025-04-23 17:21:28 +1200

    portfolio update
    featured_added_all/routes_updated

--- app.py
+++ app.py
@@ -18,5 +18,27 @@ def contactinfo():
 def portfolio():
     return render_template('portfolio.html')
 
+@app.route('/portfolio/weddings')
+def weddings():
+    return render_template('weddings.html')
+
+@app.route('/portfolio/portraits')
+def portraits():
+    return render_template('portraits.html')
+
+@app.route('/portfolio/studio')
+def studio():
+    return render_template('studio.html')
+
+@app.route('/portfolio/events')
+def events():
+    return render_template('events.html')
+
+@app.route('/portfolio/all')
+def all_portfolio():
+    return render_template('all_portfolio.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
